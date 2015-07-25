1e9615e67ce4ae262d076b5492f1040027c0da1f
Tim Bauman <tebauman@gmail.com>
2015-07-25 01:27:08 -0700

    publish website commit

--- jquery.js
+++ jquery.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
+
 	$('.dropdown').click(dropdownToggle);
+
+	$('.our-story').mouseenter(function() {
+		var pos = $('.our-story').position();
+		var width = ($('.our-story').outerWidth())/4;
+		$('.about').css('left', (pos.left - width) + "px");
+		$('.about').removeClass('hidden');
+	});
+	$('.nav').mouseleave(function() {
+			$('.about').addClass('hidden');
+		});
 });
 
-function dropdownToggle(){
-	$('#menu').toggleClass('dropdownmenu');
-	$('#menu').toggleClass('hidden');
+var dropdownToggle = function(){
+	$('.menu').toggleClass('dropdownmenu');
+	$('.menu').toggleClass('hidden');
 	$('.dropdown').toggleClass('grey');
 	$('.nav').mouseleave(dropdownToggle);
-}
+};
