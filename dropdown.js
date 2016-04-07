148c978060ea3fb7e50120993ed81230d1635938
Tim Bauman <tebauman@gmail.com>
2016-04-06 20:43:41 -0700

    fixed merge errors

--- dropdown.js
+++ dropdown.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
 	$(".statement, .statement-support").mouseenter(function() {
 		
 		var box = this.id;
+		console.log("#"+box + " .hovertext");
 		$("#"+box + " .hovertext").show();
 	});
 	$(".statement, .statement-support").mouseleave(function() {
